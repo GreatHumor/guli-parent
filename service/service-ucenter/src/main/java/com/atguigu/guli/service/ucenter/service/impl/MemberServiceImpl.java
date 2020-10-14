e8adc805c05f9245e45db917d4cb33eddebc1b5e
greathumor <silenceluo@icloud.com>
2020-10-14 23:07:23 +0800

    第三方登录-微信登录的实现

--- service/service-ucenter/src/main/java/com/atguigu/guli/service/ucenter/service/impl/MemberServiceImpl.java
+++ service/service-ucenter/src/main/java/com/atguigu/guli/service/ucenter/service/impl/MemberServiceImpl.java
@@ -77,4 +77,11 @@ public class MemberServiceImpl extends ServiceImpl<MemberMapper, Member> impleme
 
         return token;
     }
+
+    @Override
+    public Member getByOpenid(String openid) {
+        QueryWrapper<Member> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("openid",openid);
+        return baseMapper.selectOne(queryWrapper);
+    }
 }
